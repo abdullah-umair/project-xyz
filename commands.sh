fdbe736616679556e22a150fa92175b034b68d31
Abdullah Umair <abdullahumair848@gmail.com>
2023-04-01 20:59:35 +0530

    Add commands

---
+++ commands.sh
@@ -0,0 +1,11 @@
+git init 
+git add FILEPATH
+git commit -m "MESSAGE"
+git config --global user.name "Umair"
+git config --global user.email "EMAIL"
+git remote add <NAME> <URL>
+git branch -M main  
+git checkout -b feat/add-commands
+git checkout 
+git push 
+git pull
